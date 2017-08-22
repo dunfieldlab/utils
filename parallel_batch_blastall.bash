2a0c6c75bb5e1c232a1bafa07015e2e79dff1ede
nyirock <nyirock@gmail.com>
2017-08-22 12:24:52 -0600

    parallel blast adjusted

--- parallel_batch_blastall.bash
+++ parallel_batch_blastall.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-parallel -j $1 blastall "-p blastn -d nt/nt -i "{}" -o "{.}".xml -e 1e-10 -m 7 -a 1 -K 100 -b 200" ::: *.fa
+set -x
+parallel -j $1 blastall "-p blastn -d nt/nt -i "{}" -o "{.}".xml -e 1e-10 -m 7 -K 100 -b 200" ::: *.fa
